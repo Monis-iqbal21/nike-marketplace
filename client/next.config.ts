b8b74784cb685c38a58409f775edf3ec8cc11765
Monis_iqbal <monisiqbal48@gmail.com>
2025-02-13 02:10:41 +0500

    all fix with running file
    All products are showing from sanity

--- client/next.config.ts
+++ client/next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // This will allow the build to proceed even if there are ESLint errors
   },
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
 };
 
 export default nextConfig;
